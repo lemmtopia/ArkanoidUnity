be6c116212ef222d88bd3b278193cefd2eb63de0
Gabriel G. Cunha <137636549+lemmtopia@users.noreply.github.com>
2025-07-10 11:45:07 -0300

    feat: Add velocity to the ball

--- Arkanoid/Assets/Code/Scripts/BallController.cs
+++ Arkanoid/Assets/Code/Scripts/BallController.cs
@@ -2,10 +2,27 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        // Get my Rigidbody2D component
+        rb = GetComponent<Rigidbody2D>();
+
+        // Choose random X and Y sides
+        int xSide = Random.Range(-1, 1);
+        int ySide = Random.Range(-1, 1);
+
+        // Default both sides to 1 if they are 0.
+        xSide = (xSide == 0) ? 1 : xSide;
+        ySide = (ySide == 0) ? 1 : ySide;
+
+        // Create direciton vector
+        Vector2 direction = new Vector2(xSide, ySide).normalized;
+
+        // Set velocity vector
+        rb.linearVelocity = direction * moveSpeed;
     }
 
     // Update is called once per frame
